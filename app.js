02430542ac549d06d66132fe2be98add9f17c0c6
Jaspreet Singh <dexter@Jaspreets-MacBook-Pro.local>
2020-10-07 03:08:45 +0530

    tweet id change

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ mongoose.connect( process.env.mongoURI,
 
 const getTweetData = () => {
   fetch(
-  "https://api.twitter.com/2/tweets?ids=1313575229529026560&tweet.fields=public_metrics",
+  "https://api.twitter.com/2/tweets?ids=1313593776984662016&tweet.fields=public_metrics",
   {
     method: "GET",
     headers: { Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAAWEIAEAAAAApcVo%2BMmoVPNc59sjMBYSISJekYQ%3Dfy8BIhDS2Qh4Lqw1mhRqnZx88KS9bHN2wzbXDi0WvJhydUTmWg` },
